aa6680f6c7cec8dbeb8e066de8010dbc463a7d67
Will Rollitt <72352225+willrollitt@users.noreply.github.com>
2021-04-22 17:28:14 +0100

    dwm

---
+++ suckless/dwm/c/pointer_test.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	system("clear");
+
+	int x = 10;
+	printf("x equals %d\n", x);
+	
+	int *aptr = &x;
+	
+	printf("apter is %x\n", aptr);
+
+	return 0;
+
+}
