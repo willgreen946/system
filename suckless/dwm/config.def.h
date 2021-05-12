91cbb356d09c7b4ec57558377bb05f2b06302d7c
Will Rollitt <72352225+willrollitt@users.noreply.github.com>
2021-05-12 20:16:25 +0100

    new bindings

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -68,14 +68,24 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *browser[] = { "firefox", NULL };
+static const char *termcmd[]  =  { "st", NULL };
+static const char *browser[] =   { "firefox", NULL };
+static const char *ranger[] =    { "st", "-e", "ranger"};
+static const char *ncpamixer[] = { "st", "-e", "ncpamixer"};
+static const char *wifi[] =      { "st", "-e", "connman-ncurses" };
+static const char *htop[] =	 { "st", "-e", "htop" };
+static const char *wifigtk[] =   { "connman-gtk", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY|ControlMask,           XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ControlMask,           XK_Return, spawn,          {.v = termcmd  } },
 	{ MODKEY|ControlMask,		XK_space,  spawn,	   {.v = browser  } },
+	{ MODKEY|ControlMask,		XK_r,	   spawn,	   {.v = ranger   } },
+	{ MODKEY|ControlMask,		XK_a,	   spawn,	   {.v = ncpamixer} },
+	{ MODKEY|ControlMask,		XK_w,	   spawn,	   {.v = wifi 	  } },
+	{ MODKEY|ControlMask,		XK_c,	   spawn,	   {.v = wifigtk  } },
+	{ MODKEY|ControlMask,		XK_h,	   spawn,	   {.v = htop     } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
