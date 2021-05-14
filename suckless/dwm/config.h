86ae63e2d88f8dbbc89db1b57410ac0f559e8f45
Will Rollitt <72352225+willrollitt@users.noreply.github.com>
2021-05-14 07:25:01 +0100

    green

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -11,7 +11,7 @@ static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#000000";
 static const char col_gray3[]       = "#ffffff";
 static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#cd0000";
+static const char col_cyan[]        = "#03a062";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
