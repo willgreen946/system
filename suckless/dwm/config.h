261ad1d5d5c15dec8ae85292d61aab28c868645e
Will Rollitt <72352225+willrollitt@users.noreply.github.com>
2021-05-16 16:18:22 +0000

    changed browser to gnu icecat

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -70,7 +70,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  =  { "st", NULL };
-static const char *browser[] =   { "firefox", NULL };
+static const char *browser[] =   { "icecat", NULL };
 static const char *wifigtk[] =   { "connman-gtk", NULL};
 
 //Terminal Applications
