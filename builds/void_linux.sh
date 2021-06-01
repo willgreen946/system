d3c284df1de14e4ea1e915d05547111493238cad
Will Rollitt <72352225+willrollitt@users.noreply.github.com>
2021-06-01 20:11:02 +0000

    Update void_linux.sh

--- builds/void_linux.sh
+++ builds/void_linux.sh
@@ -2,7 +2,7 @@
 xbps-install -Sy gcc base-devel pkgconfig pkgconf libX11-devel libXft-devel libXinermama-devel ncurses-devel
 
 # X11/Wayland window managers
-xbps-install -Sy xorg-minimal i3
+xbps-install -Sy xorg-minimal i3 sway elogind
 
 # X11 Programs
 xbps-install -Sy setxkbmap xrdb xdotool nitrogen rxvt-unicode xsel scrot feh connman-gtk
@@ -11,7 +11,10 @@ xbps-install -Sy setxkbmap xrdb xdotool nitrogen rxvt-unicode xsel scrot feh con
 xbps-install -Sy firefox icecat elinks lynx tor torbrowser-launcher
 
 # Internet
-xbps-install -Sy mutt connman connman-ncurses onionshare torsocks
+xbps-install -Sy mutt connman connman-ncurses onionshare torsocks dbus
+
+# Audio
+xbps-install -Sy pulseaudio ncpamixer
 
 # System tools
 xbps-install -Sy sudo tmux git make zsh bash tty-clock acpi
@@ -21,6 +24,10 @@ xbps-install -Sy vim vifm vimpc vimiv vimb hexer cointop
 
 # Misc
 xbps-install -Sy neofetch dialog 
+
+# Enable things at boot
+ln -s /etc/sv/dbus /var/service
+ln -s /etc/sv/connmand /var/service
 
 # Vi/Vim like htop
 git clone https://github.com/KoffeinFlummi/htop-vim.git
