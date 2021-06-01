05ff6554e6d5298a4d3e784d438c38cab6d510fe
Will Rollitt <72352225+willrollitt@users.noreply.github.com>
2021-06-01 20:08:06 +0000

    new

--- builds/void_linux.sh
+++ builds/void_linux.sh
@@ -2,19 +2,26 @@
 xbps-install -Sy gcc base-devel pkgconfig pkgconf libX11-devel libXft-devel libXinermama-devel ncurses-devel
 
 # X11/Wayland window managers
-xbps-install -Sy xorg-minimal i3 sway elogind mesa-dri 
+xbps-install -Sy xorg-minimal i3
 
 # X11 Programs
 xbps-install -Sy setxkbmap xrdb xdotool nitrogen rxvt-unicode xsel scrot feh connman-gtk
 
 # Web browsers
-xbps-install -Sy firefox icecat vimb elinks lynx tor torbrowser-launcher
+xbps-install -Sy firefox icecat elinks lynx tor torbrowser-launcher
 
 # Internet
-xbps-install -Sy connman connman-ncurses onionshare torsocks
+xbps-install -Sy mutt connman connman-ncurses onionshare torsocks
 
 # System tools
-xbps-install -Sy sudo tmux vifm vim git make zsh bash tty-clock acpi
+xbps-install -Sy sudo tmux git make zsh bash tty-clock acpi
+
+# Vi/Vim like programs
+xbps-install -Sy vim vifm vimpc vimiv vimb hexer cointop 
 
 # Misc
 xbps-install -Sy neofetch dialog 
+
+# Vi/Vim like htop
+git clone https://github.com/KoffeinFlummi/htop-vim.git
+./autogen.sh && ./configure && make 
