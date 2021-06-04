d927e55695232a025409e5457986e19cd580dc36
Will Rollitt <72352225+willrollitt@users.noreply.github.com>
2021-06-04 20:55:26 +0000

    Add files via upload

---
+++ build_suckless.sh
@@ -0,0 +1,23 @@
+cd suckless
+
+cd dwm
+make clean install
+cd ..
+
+cd dmenu
+make clean install
+cd ..
+
+cd st
+make clean install
+cd ..
+
+cd slstatus
+make clean install
+cd ..
+
+cd dwl
+make clean install
+cd ..
+
+cd ..
